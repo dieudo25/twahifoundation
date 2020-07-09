29063b45881524cf41c940f29c46135b90600af9
Dieudo25 <twahirwa25@gmail.com>
2020-07-09 18:43:21 +0200

    create script dumpdata and loaddata for model + runserverver script

---
+++ twahifoundation/scripts/dumpdata.sh
@@ -0,0 +1,194 @@
+#!/bin/bash
+
+# Check if the script has been executed from the twahifoundation/ directory
+# Move the current directory to it if not in order to execute the script
+
+FILE=../manage.py
+if test -f "$FILE"; then
+    cd ..
+fi
+
+
+# dump all the data from the account app and django.contrib.auth module in the twahifoundation/account/fixtures directory with the manage.py utility
+
+python3 manage.py dumpdata auth.group --indent 4 > account/fixtures/group.json
+status=$?
+
+if [ $status -ne 0 ] ;then
+    printf "(0_group.json)\n>>> ERROR: JSON-data export has failed  (Fixture import error: $status)\n"
+    printf "You may check if the data is valid and put in 'twahifoundation/account/fixtures'"
+    exit $status
+else
+    printf "(0_group.json)\n>>> SUCCESS: Fixture JSON-data successfully exported\n"
+fi
+
+
+python3 manage.py dumpdata auth.permission --indent 4 > account/fixtures/permission.json
+status=$?
+
+if [ $status -ne 0 ] ;then
+    printf "(1_permission.json)\n>>> ERROR: JSON-data export has failed  (Fixture import error: $status)\n"
+    printf "You may check if the data is valid and put in 'twahifoundation/account/fixtures'"
+    exit $status
+else
+    printf "(1_permission.json)\n>>> SUCCESS: Fixture JSON-data successfully exported\n"
+fi
+
+python3 manage.py dumpdata account.user --indent 4 > account/fixtures/user.json
+status=$?
+
+if [ $status -ne 0 ] ;then
+    printf "(2_user.json)\n>>> ERROR: JSON-data export has failed  (Fixture import error: $status)\n"
+    printf "You may check if the data is valid and put in 'twahifoundation/account/fixtures'"
+    exit $status
+else
+    printf "(2_user.json)\n>>> SUCCESS: Fixture JSON-data successfully exported\n"
+fi
+
+
+
+# dump all the data from the project app in the twahifoundation/project/fixtures directory with the manage.py utility
+
+python3 manage.py dumpdata project.project --indent 4 > project/fixtures/project.json
+status=$?
+
+if [ $status -ne 0 ] ;then
+    printf "(3_project.json)\n>>> ERROR: JSON-data export has failed  (Fixture import error: $status)\n"
+    printf "You may check if the data is valid and put in 'twahifoundation/project/fixtures'"
+    exit $status
+else
+    printf "(3_project.json)\n>>> SUCCESS: Fixture JSON-data successfully exported\n"
+fi
+
+python3 manage.py dumpdata project.event --indent 4 > project/fixtures/event.json
+status=$?
+
+if [ $status -ne 0 ] ;then
+    printf "(4_event.json)\n>>> ERROR: JSON-data export has failed  (Fixture import error: $status)\n"
+    printf "You may check if the data is valid and put in 'twahifoundation/project/fixtures'"
+    exit $status
+else
+    printf "(4_event.json)\n>>> SUCCESS: Fixture JSON-data successfully exported\n"
+fi
+
+python3 manage.py dumpdata project.task --indent 4 > project/fixtures/task.json
+status=$?
+
+if [ $status -ne 0 ] ;then
+    printf "(5_task.json)\n>>> ERROR: JSON-data export has failed  (Fixture import error: $status)\n"
+    printf "You may check if the data is valid and put in 'twahifoundation/project/fixtures'"
+    exit $status
+else
+    printf "(5_task.json)\n>>> SUCCESS: Fixture JSON-data successfully exported\n"
+fi
+
+
+# dump all the data from the transaction app in the twahifoundation/transaction/fixtures directory with the manage.py utility
+
+python3 manage.py dumpdata transaction.transaction --indent 4 > transaction/fixtures/transaction.json
+status=$?
+
+if [ $status -ne 0 ] ;then
+    printf "(6_donnatingtransaction.json)\n>>> ERROR: JSON-data export has failed  (Fixture import error: $status)\n"
+    printf "You may check if the data is valid and put in 'twahifoundation/transaction/fixtures'"
+    exit $status
+else
+    printf "(6_donnatingtransaction.json)\n>>> SUCCESS: Fixture JSON-data successfully exported\n"
+fi
+
+python3 manage.py dumpdata transaction.producttransactionline --indent 4 > transaction/fixtures/product_transaction_line.json
+status=$?
+
+if [ $status -ne 0 ] ;then
+    printf "(7_product_transaction_line.json)\n>>> ERROR: JSON-data export has failed  (Fixture import error: $status)\n"
+    printf "You may check if the data is valid and put in 'twahifoundation/transaction/fixtures'"
+    exit $status
+else
+    printf "(7_product_transaction_line.json)\n>>> SUCCESS: Fixture JSON-data successfully exported\n"
+fi
+
+
+# dump all the data from the stock app in the twahifoundation/stock/fixtures directory with the manage.py utility
+
+python3 manage.py dumpdata stock.product --indent 4 > stock/fixtures/product.json
+status=$?
+
+if [ $status -ne 0 ] ;then
+    printf "(8_product.json)\n>>> ERROR: JSON-data export has failed  (Fixture import error: $status)\n"
+    printf "You may check if the data is valid and put in 'twahifoundation/stock/fixtures'"
+    exit $status
+else
+    printf "(8_product.json)\n>>> SUCCESS: Fixture JSON-data successfully exported\n"
+fi
+
+python3 manage.py dumpdata stock.category --indent 4 > stock/fixtures/category.json
+status=$?
+
+if [ $status -ne 0 ] ;then
+    printf "(9_category.json)\n>>> ERROR: JSON-data export has failed  (Fixture import error: $status)\n"
+    printf "You may check if the data is valid and put in 'twahifoundation/stock/fixtures'"
+    exit $status
+else
+    printf "(9_category.json)\n>>> SUCCESS: Fixture JSON-data successfully exported\n"
+fi
+
+python3 manage.py dumpdata stock.stock --indent 4 > stock/fixtures/stock.json
+status=$?
+
+if [ $status -ne 0 ] ;then
+    printf "(10_stock.json)\n>>> ERROR: JSON-data export has failed  (Fixture import error: $status)\n"
+    printf "You may check if the data is valid and put in 'twahifoundation/stock/fixtures'"
+    exit $status
+else
+    printf "(10_stock.json)\n>>> SUCCESS: Fixture JSON-data successfully exported\n"
+fi
+
+python3 manage.py dumpdata stock.productstocktransfert --indent 4 > stock/fixtures/product_stock_transfert.json
+status=$?
+
+if [ $status -ne 0 ] ;then
+    printf "(11_product_stock_transfert.json)\n>>> ERROR: JSON-data export has failed  (Fixture import error: $status)\n"
+    printf "You may check if the data is valid and put in 'twahifoundation/stock/fixtures'"
+    exit $status
+else
+    printf "(11_product_stock_transfert.json)\n>>> SUCCESS: Fixture JSON-data successfully exported\n"
+fi
+
+
+# dump all the data from the contact app in the twahifoundation/contact/fixtures directory with the manage.py utility
+
+python3 manage.py dumpdata contact.person --indent 4 > contact/fixtures/person.json
+status=$?
+
+if [ $status -ne 0 ] ;then
+    printf "(12_person.json)\n>>> ERROR: JSON-data export has failed  (Fixture import error: $status)\n"
+    printf "You may check if the data is valid and put in 'twahifoundation/contact/fixtures'"
+    exit $status
+else
+    printf "(12_person.json)\n>>> SUCCESS: Fixture JSON-data successfully exported\n"
+fi
+
+python3 manage.py dumpdata contact.company --indent 4 > contact/fixtures/company.json
+status=$?
+
+if [ $status -ne 0 ] ;then
+    printf "(13_company.json)\n>>> ERROR: JSON-data export has failed  (Fixture import error: $status)\n"
+    printf "You may check if the data is valid and put in 'twahifoundation/contact/fixtures'"
+    exit $status
+else
+    printf "(13_company.json)\n>>> SUCCESS: Fixture JSON-data successfully exported\n"
+fi
+
+
+# dump all the data from the flatpage app in the twahifoundation/flatpage/fixtures directory with the manage.py utility
+
+#python3 manage.py dumpdata flatpage.customflatpage --indent 4 > flatpage/fixtures/custom_flatpage.json
+#status=$?
+#
+#if [ $status -ne 0 ] ;then
+#    printf "(6_customflatpage.json)\n>>> ERROR: JSON-data export has failed  (Fixture import error: $status)\n"
+#    printf "You may check if the data is valid and put in 'twahifoundation/flatpage/fixtures'"
+#    exit $status
+#else
+#    printf "(6_customflatpage.json)\n>>> SUCCESS: Fixture JSON-data successfully exported\n"
+#fi
---
+++ twahifoundation/scripts/loaddata.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+
+# Check if the script has been executed from the twahifoundation/ directory
+# Move the current directory to it if not in order to execute the script
+
+FILE=../manage.py
+if test -f "$FILE"; then
+    cd ..
+fi
+
+
+# Loads all the JSON-fixtures available in the twahifoundation/account/fixtures directory with the manage.py utility
+
+for file in account/fixtures/*
+do
+    python3 manage.py loaddata $file
+    status=$?
+
+    if [ $status -ne 0 ] ;then
+        printf "($file >>> Fixture import error: $status)\n>>> ERROR: JSON-data import has failed\n"
+        printf "You may check if the data is valid and put in 'twahifoundation/account/fixtures'"
+        exit $status
+    else
+        printf "($file)\n>>> SUCCESS: Fixture JSON-data successfully imported\n"
+    fi
+done
+
+
+# Loads all the JSON-fixtures available in the twahifoundation/project/fixtures directory with the manage.py utility
+
+for file in project/fixtures/*
+do
+    python3 manage.py loaddata $file
+    status=$?
+
+    if [ $status -ne 0 ] ;then
+        printf "($file >>> Fixture import error: $status)\n>>> ERROR: JSON-data import has failed\n"
+        printf "You may check if the data is valid and put in 'twahifoundation/project/fixtures'"
+        exit $status
+    else
+        printf "($file)\n>>> SUCCESS: Fixture JSON-data successfully imported\n"
+    fi
+done
+
+
+# Loads all the JSON-fixtures available in the twahifoundation/transaction/fixtures directory with the manage.py utility
+
+for file in transaction/fixtures/*
+do
+    python3 manage.py loaddata $file
+    status=$?
+
+    if [ $status -ne 0 ] ;then
+        printf "($file >>> Fixture import error: $status)\n>>> ERROR: JSON-data import has failed\n"
+        printf "You may check if the data is valid and put in 'twahifoundation/transaction/fixtures'"
+        exit $status
+    else
+        printf "($file)\n>>> SUCCESS: Fixture JSON-data successfully imported\n"
+    fi
+done
+
+printf "\n------------------------------------------------------\n SUCCESS: All fixture JSON-data successfully imported \n------------------------------------------------------\n"
+exit 0
+
+# Loads all the JSON-fixtures available in the twahifoundation/stock/fixtures directory with the manage.py utility
+
+for file in stock/fixtures/*
+do
+    python3 manage.py loaddata $file
+    status=$?
+
+    if [ $status -ne 0 ] ;then
+        printf "($file >>> Fixture import error: $status)\n>>> ERROR: JSON-data import has failed\n"
+        printf "You may check if the data is valid and put in 'twahifoundation/stock/fixtures'"
+        exit $status
+    else
+        printf "($file)\n>>> SUCCESS: Fixture JSON-data successfully imported\n"
+    fi
+done
+
+printf "\n------------------------------------------------------\n SUCCESS: All fixture JSON-data successfully imported \n------------------------------------------------------\n"
+exit 0
+
+# Loads all the JSON-fixtures available in the twahifoundation/contact/fixtures directory with the manage.py utility
+
+for file in contact/fixtures/*
+do
+    python3 manage.py loaddata $file
+    status=$?
+
+    if [ $status -ne 0 ] ;then
+        printf "($file >>> Fixture import error: $status)\n>>> ERROR: JSON-data import has failed\n"
+        printf "You may check if the data is valid and put in 'twahifoundation/contact/fixtures'"
+        exit $status
+    else
+        printf "($file)\n>>> SUCCESS: Fixture JSON-data successfully imported\n"
+    fi
+done
+
+
+# Loads all the JSON-fixtures available in the twahifoundation/flatpage/fixtures directory with the manage.py utility
+
+for file in flatpage/fixtures/*
+do
+    python3 manage.py loaddata $file
+    status=$?
+
+    if [ $status -ne 0 ] ;then
+        printf "($file >>> Fixture import error: $status)\n>>> ERROR: JSON-data import has failed\n"
+        printf "You may check if the data is valid and put in 'twahifoundation/flatpage/fixtures'"
+        exit $status
+    else
+        printf "($file)\n>>> SUCCESS: Fixture JSON-data successfully imported\n"
+    fi
+done
