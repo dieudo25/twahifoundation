250594075a916f5fb9dc119c3693ee464c737493
Dieudo25 <twahirwa25@gmail.com>
2020-07-14 20:18:27 +0200

    cretate homepage

--- twahifoundation/page/views.py
+++ twahifoundation/page/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class Home(TemplateView):
+    "Home page"
+
+    template_name = 'page/home.html'
