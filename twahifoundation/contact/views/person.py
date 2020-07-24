a40ad0fd7d0f75624425a07ae4e08ade4e2cec1f
Dieudo25 <twahirwa25@gmail.com>
2020-07-24 20:52:21 +0200

    update contact app : add PersonListFiltered

--- twahifoundation/contact/views/person.py
+++ twahifoundation/contact/views/person.py
@@ -11,3 +11,20 @@ class PersonListView(ListView):
     model = Person
     template_name = 'contact/person/list.html'
     context_object_name = 'person_list'
+
+
+class PersonListFiltered(ListView):
+    "Peron list filterd by email, lastname or firstname"
+
+    model = Person
+    template_name = 'contact/person/list_filtered.html'
+    context_object_name = 'filtered_person_list'
+
+    def get_queryset(self):
+        query = self.request.GET.get('search')
+        object_list = Person.objects.filter(
+            Q(email__icontains=query) |
+            Q(first_name__icontains=query) |
+            Q(last_name__icontains=query)
+        )
+        return object_list
