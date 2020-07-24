6bd3f85f16e62dd45a0b1c6efe4635a7721772a7
Dieudo25 <twahirwa25@gmail.com>
2020-07-24 20:50:22 +0200

    contact migrations + update Person and Contact models

--- twahifoundation/contact/admin/person.py
+++ twahifoundation/contact/admin/person.py
@@ -4,11 +4,11 @@ from contact.models.person import Person, Company
 
 
 class PersonAdmin(admin.ModelAdmin):
-    pass
+    readonly_fields = ["slug"]
 
 
 class CompanyAdmin(admin.ModelAdmin):
-    pass
+    readonly_fields = ["slug"]
 
 
 admin.site.register(Person, PersonAdmin)
--- twahifoundation/contact/models/person.py
+++ twahifoundation/contact/models/person.py
@@ -1,4 +1,8 @@
+import itertools
+
 from django.db import models
+from django.urls import reverse
+from django.utils.text import slugify
 
 
 class Company(models.Model):
@@ -6,31 +10,73 @@ class Company(models.Model):
     Company model definition
     """
 
-    name = models.CharField(
-        max_length=60, verbose_name="Nom", default=None)
-    adresse = models.CharField(
-        max_length=60, verbose_name="Adresse", default=None)
+    name = models.CharField(max_length=60, default=None)
+    address = models.CharField(
+        max_length=60,
+        default=None
+    )
     email = models.EmailField(max_length=254, default=None)
     phone_number = models.CharField(
-        max_length=60, verbose_name="Téléphone", null=None)
-    site_web = models.URLField(max_length=200, blank=True)
+        max_length=60,
+        null=None
+    )
+    website = models.URLField(max_length=200, blank=True)
     is_partner = models.BooleanField(
-        default=False, verbose_name="Est un partenaire")
+        default=False, verbose_name="Is a partner")
+    slug = models.SlugField(max_length=60, unique=True)
 
     class Meta:
         """
         Meta definition for Company.
         """
 
-        verbose_name = 'Entreprise'
-        verbose_name_plural = 'Entreprises'
+        verbose_name_plural = 'Companies'
+
+    def _generate_slug(self):
+        """
+        Generate a slug based on the name of the company
+
+        If the slug is already taken, one or two digits will be added at the
+        end of the slug and will increment as long as the slug already exist
+        until reaching a non-existant result.
+        The slug is truncated to 57 character in order to add the unique digits
+        at the end of it.
+        """
+
+        max_length = self._meta.get_field('slug').max_length - 3
+        value = self.name
+        slug_result = slug_original = \
+            slugify(value, allow_unicode=False)[:max_length]
+
+        for i in itertools.count(1):
+            if not Company.objects.filter(slug=slug_result).exists():
+                break
+            slug_result = f'{slug_original}-{i}'
+
+        self.slug = slug_result
 
     def __str__(self):
         """
-        Unicode representation of Entreprise.
+        Unicode representation of Company.
         """
 
         return f"{self.name}"
+
+    """ def get_absolute_url(self):
+        "Get the absolute url of the object"
+        return reverse("account:user-detail", kwargs={"slug": self.slug}) """
+
+    def save(self, *args, **kwargs):
+        """
+        Save method for User.
+
+        Generate a slug based on the username if the user doesn't exist yet.-
+        """
+
+        if not self.pk:
+            self._generate_slug()
+
+        super().save(*args, **kwargs)
 
 
 class Person(models.Model):
@@ -40,30 +86,70 @@ class Person(models.Model):
     company = models.ForeignKey(
         Company, on_delete=models.CASCADE, null=True, blank=True)
     first_name = models.CharField(
-        max_length=60, verbose_name="Prénom", default=None)
+        max_length=60, default=None)
     last_name = models.CharField(
-        max_length=60, verbose_name="Nom", default=None)
+        max_length=60, default=None)
     email = models.EmailField(max_length=254, default=None)
     phone_number = models.CharField(
-        max_length=60, verbose_name="Téléphone", null=None)
+        max_length=60, null=None)
     is_supplier = models.BooleanField(
-        default=False, verbose_name="Est un fournisseur")
+        default=False, verbose_name="Is a supplier")
     is_donor = models.BooleanField(
-        default=False, verbose_name="Est un donateur")
+        default=False, verbose_name="Is a donor")
     is_follower = models.BooleanField(
-        default=False, verbose_name="Est inscrit à la newsletter")
+        default=False, verbose_name="Is subscribed to the newsletter")
+    slug = models.SlugField(max_length=60, unique=True)
 
     class Meta:
         """
         Meta definition for Person.
         """
 
-        verbose_name = 'Contact'
-        verbose_name_plural = 'Contacts'
+        verbose_name = 'Individual'
+        verbose_name_plural = 'Individuals'
 
     def __str__(self):
         """
-        Unicode representation of Donor.
+        Unicode representation of Person.
         """
 
         return f"{self.last_name} {self.first_name}"
+
+    def _generate_slug(self):
+        """
+        Generate a slug based on the firstname and lastname of the person
+
+        If the slug is already taken, one or two digits will be added at the
+        end of the slug and will increment as long as the slug already exist
+        until reaching a non-existant result.
+        The slug is truncated to 57 character in order to add the unique digits
+        at the end of it.
+        """
+
+        max_length = self._meta.get_field('slug').max_length - 3
+        value = f'{self.first_name} {self.last_name}'
+        slug_result = slug_original = \
+            slugify(value, allow_unicode=False)[:max_length]
+
+        for i in itertools.count(1):
+            if not Person.objects.filter(slug=slug_result).exists():
+                break
+            slug_result = f'{slug_original}-{i}'
+
+        self.slug = slug_result
+
+    """ def get_absolute_url(self):
+        "Get the absolute url of the object"
+        return reverse("account:user-detail", kwargs={"slug": self.slug}) """
+
+    def save(self, *args, **kwargs):
+        """
+        Save method for Person.
+
+        Generate a slug based on the firstname and lastname if the person doesn't exist yet.-
+        """
+
+        if not self.pk:
+            self._generate_slug()
+
+        super().save(*args, **kwargs)
