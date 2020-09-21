aabb31fcd326f985e257c0c53cda42883ba9459b
dieudo25 <twahirwa25@gmail.com>
2020-09-21 21:29:57 +0000

    update

--- twahifoundation/portal/urls.py
+++ twahifoundation/portal/urls.py
@@ -10,13 +10,13 @@ from portal.views.views import (
 app_name = 'portal'
 
 urlpatterns = [
-    re_path(r'notifications/read/all',
+    re_path(r'notifications/read/all/$',
             mark_all_as_read, name="notice-read-all"),
     re_path(r'notifications/(?P<pk>\d+)/read',
             mark_as_read, name="notice-read"),
-    re_path(r'^notifications/', NotificationList.as_view(),
+    re_path(r'^notifications/$', NotificationList.as_view(),
             name="user-notifications"),
-    re_path(r'', Home.as_view(), name="portal-home"),
+    re_path(r'^$', Home.as_view(), name="portal-home"),
 
 
 ]
