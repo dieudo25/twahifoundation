2cf8bc429952185c123196c550b2159ad45eda31
dieudo25 <twahirwa25@gmail.com>
2020-09-26 13:01:08 +0000

    upddate

--- twahifoundation/page/views/language.py
+++ twahifoundation/page/views/language.py
@@ -1,15 +1,14 @@
 from django.conf import settings
+from django.utils import translation
 from django.http import HttpResponseRedirect
 
 
 def change_language(request, language):
     response = HttpResponseRedirect('/')
 
-    if language == 'fr':
+    if language:
         redirect_path = f'/{language}/'
-    else:
-        redirect_path = '/'
-    from django.utils import translation
+
     translation.activate(language)
     response = HttpResponseRedirect(redirect_path)
     response.set_cookie(settings.LANGUAGE_COOKIE_NAME, language)
