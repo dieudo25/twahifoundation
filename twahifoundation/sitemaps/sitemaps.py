d0ac37c0123990343bf77cbf165dc84d2e57d14c
Dieudo25 <twahirwa25@gmail.com>
2020-11-04 00:08:21 +0100

    update

--- twahifoundation/sitemaps/sitemaps.py
+++ twahifoundation/sitemaps/sitemaps.py
@@ -8,23 +8,50 @@ from project.models.project import Project
 
 class PostSitemap(Sitemap):
 
+    changefreq = "monthly"
+    priority = 0.5
+    protocol = "https"
+
+
     def items(self):
         return Post.objects.filter(
             Q(status='Published') |
             Q(category__name='Page')
         )
+    
+    def lastmod(self, obj):
+        if obj.updated_on:
+            return obj.updated_on
+        else:
+            return obj.created_on
+
 
 class EventSitemap(Sitemap):
+    changefreq = "monthly"
+    priority = 0.5
+    protocol = "https"
 
     def items(self):
         return Event.objects.filter(status='Published')
 
+    def lastmod(self, obj):
+        return obj.date_created
+
 class ProjectSitemap(Sitemap):
+    changefreq = "monthly"
+    priority = 0.5
+    protocol = "https"   
 
     def items(self):
         return Project.objects.filter(status='Published')
+    
+    def lastmod(self, obj):
+        return obj.date_created
 
 class StaticViewSitemap(Sitemap):
+    changefreq = "monthly"
+    priority = 0.5
+    protocol = "https"   
 
     def items(self):
         return [
